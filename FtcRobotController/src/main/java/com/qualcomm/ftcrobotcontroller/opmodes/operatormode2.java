1c6c72c93b261e479b72977992ded7a448d7c8a5
whitmorelake8492 <whitmore.robot.8492@gmail.com>
2015-10-28 19:18:17 -0400

    added servo code

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/operatormode2.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/operatormode2.java
@@ -3,21 +3,21 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
-
+import com.qualcomm.robotcore.hardware.Servo;
 /**
  * Created by techclub on 9/12/15.
  */
 public class operatormode2 extends OpMode {
-   private DcMotorController dc_drive_controller;
+    private DcMotorController dc_drive_controller;
 
     private DcMotor dc_drive_left;
-
+    private Servo servo1;
     private DcMotor dc_drive_right;
 
     @Override
     public void init() {
         dc_drive_controller = hardwareMap.dcMotorController.get("drive_controller");
-
+        servo1 = hardwareMap.servo.get("servo1");
         dc_drive_left = hardwareMap.dcMotor.get("left_drive");
 
         dc_drive_right = hardwareMap.dcMotor.get("right_drive");
@@ -31,5 +31,14 @@ public class operatormode2 extends OpMode {
         dc_drive_left.setPower(gamepad1.left_stick_y);
 
         dc_drive_right.setPower(gamepad1.right_stick_y);
+        if (gamepad2.x) {
+            servo1.setPosition(1.0);
+        }
+        dc_drive_right.setPower(gamepad1.right_stick_y);
+        if (gamepad2.a) {
+            servo1.setPosition(0.0);
+
+        }
+
     }
 }
