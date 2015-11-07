2279e3cf3940afb92392c3f2b674243bc98d08c8
whitmorelake8492 <whitmore.robot.8492@gmail.com>
2015-11-06 20:15:49 -0500

    kitten-- added 4link code

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/operatormode2.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/operatormode2.java
@@ -13,38 +13,42 @@ public class operatormode2 extends OpMode {
     private DcMotor dc_drive_left;
     private Servo servo1;
     private DcMotor dc_drive_right;
+    private DcMotor dc_4link;
     private TwoMotorDrive leftWheel;
+    private int fourLinkUpper = 1920;
+    private int fourLinkLower = 0;
     @Override
     public void init() {
         dc_drive_controller = hardwareMap.dcMotorController.get("drive_controller");
         servo1 = hardwareMap.servo.get("servo1");
         dc_drive_left = hardwareMap.dcMotor.get("left_drive");
-
+        dc_4link = hardwareMap.dcMotor.get("4link");
         dc_drive_right = hardwareMap.dcMotor.get("right_drive");
 
         dc_drive_left.setDirection(DcMotor.Direction.REVERSE);
         leftWheel = new TwoMotorDrive(dc_drive_left, dc_drive_right);
+        dc_4link.setChannelMode
+                ( DcMotorController.RunMode.RUN_USING_ENCODERS
+                );
 
     }
 
     @Override
     public void loop() {
-        dc_drive_left.setPower(gamepad1.left_stick_y);
-
-        dc_drive_right.setPower(gamepad1.right_stick_y);
-
-
-        if (gamepad2.x) {
-            servo1.setPosition(0.0);
-        }
-
-
-
-        dc_drive_right.setPower(gamepad1.right_stick_y);
-        if (gamepad2.a) {
+        if (gamepad2.a && servo1.getPosition()!=1.0) {
             servo1.setPosition(1.0);
 
         }
-        leftWheel.setPower(gamepad2.left_stick_y);
+        if (gamepad2.a && servo1.getPosition()!=0.0) {
+            servo1.setPosition(0.0);
+
+        }
+        if ( (dc_4link.getCurrentPosition()<=fourLinkLower && gamepad2.right_stick_y > 0.0 ) ||
+        (dc_4link.getCurrentPosition()>=fourLinkUpper && gamepad2.right_stick_y < 0.0) ||
+                (dc_4link.getCurrentPosition()< fourLinkUpper && dc_4link.getCurrentPosition()>fourLinkLower)){
+            dc_4link.setPower(gamepad2.right_stick_y);
+
+        }
+        leftWheel.setPower(gamepad1.left_stick_y);
     }
 }
