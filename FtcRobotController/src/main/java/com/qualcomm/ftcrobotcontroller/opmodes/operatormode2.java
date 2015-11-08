44fc9a4fa1e69def197ab2d447e5b05a0e53312d
whitmorelake8492 <whitmore.robot.8492@gmail.com>
2015-11-07 20:14:34 -0500

    DRZ-- added acc. contorls

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/operatormode2.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/operatormode2.java
@@ -4,14 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
+
 /**
  * Created by techclub on 9/12/15.
  */
 public class operatormode2 extends OpMode {
     private DcMotorController dc_drive_controller;
-
+    private ServoController servoController;
     private DcMotor dc_drive_left;
-    private Servo servo1;
+    private Servo bucket_servo;
+    private Servo left_slide;
+    private Servo right_slide;
     private DcMotor dc_drive_right;
     private DcMotor dc_4link;
     private TwoMotorDrive rightWheel;
@@ -20,15 +24,21 @@ public class operatormode2 extends OpMode {
     private int fourLinkLower = 0;
     private DcMotor dc_drive_right2;
     private DcMotor dc_drive_left2;
+    private DcMotor dc_sweeper;
+
     @Override
     public void init() {
         dc_drive_controller = hardwareMap.dcMotorController.get("drive_controller");
-        servo1 = hardwareMap.servo.get("servo1");
+        servoController = hardwareMap.servoController.get("servoController");
+        bucket_servo = hardwareMap.servo.get("bucket_servo");
+        left_slide = hardwareMap.servo.get("left_slide");
+        right_slide = hardwareMap.servo.get("right_slide");
         dc_drive_left = hardwareMap.dcMotor.get("left_drive");
         dc_4link = hardwareMap.dcMotor.get("4link");
         dc_drive_right = hardwareMap.dcMotor.get("right_drive");
         dc_drive_right2 = hardwareMap.dcMotor.get("right_drive2");
         dc_drive_left2 = hardwareMap.dcMotor.get("left_drive2");
+        dc_sweeper = hardwareMap.dcMotor.get("sweeper");
         dc_drive_left2.setDirection(DcMotor.Direction.REVERSE);
         dc_drive_left.setDirection(DcMotor.Direction.REVERSE);
         leftWheel = new TwoMotorDrive(dc_drive_left, dc_drive_left2);
@@ -41,20 +51,45 @@ public class operatormode2 extends OpMode {
 
     @Override
     public void loop() {
-        if (gamepad2.a && servo1.getPosition()!=1.0) {
-            servo1.setPosition(1.0);
+        if (gamepad2.a && bucket_servo.getPosition()!=0.5) { //sets bucket to normal with a button
+            bucket_servo.setPosition(0.5);
+        }
+        if (gamepad2.a && bucket_servo.getPosition()!=0.25) { //sets bucket to dump left with a button
+            bucket_servo.setPosition(0.25);
 
         }
-        if (gamepad2.a && servo1.getPosition()!=0.0) {
-            servo1.setPosition(0.0);
+
+        if (gamepad2.y && bucket_servo.getPosition()!=0.5) { //sets bucket to normal with y button
+            bucket_servo.setPosition(0.5);
+        }
+        if (gamepad2.y && bucket_servo.getPosition()!=0.75) { //sets bucket to dump right with y button
+            bucket_servo.setPosition(0.75);
 
         }
-        if ( (dc_4link.getCurrentPosition()<=fourLinkLower && gamepad2.right_stick_y > 0.0 ) ||
-        (dc_4link.getCurrentPosition()>=fourLinkUpper && gamepad2.right_stick_y < 0.0) ||
-                (dc_4link.getCurrentPosition()< fourLinkUpper && dc_4link.getCurrentPosition()>fourLinkLower)){
+
+        if (gamepad2.x && left_slide.getPosition()!=0.0) { //sets left slide to in with x button
+            left_slide.setPosition(0.0);
+        }
+        if (gamepad2.x && left_slide.getPosition()!=0.3) { //sets left slide to out with x button
+            left_slide.setPosition(0.3);
+
+        }
+
+        if (gamepad2.b && right_slide.getPosition()!=0.0) { //sets right slide to in with b button
+            right_slide.setPosition(0.0);
+        }
+        if (gamepad2.b && right_slide.getPosition()!=0.3) { //sets right slide to out with b button
+            right_slide.setPosition(0.3);
+
+        }
+
+        if ( (dc_4link.getCurrentPosition()<=fourLinkLower && gamepad2.right_stick_y > 0.0 ) ||                 //
+        (dc_4link.getCurrentPosition()>=fourLinkUpper && gamepad2.right_stick_y < 0.0) ||                       //Crazy Long 'If' statement 2.0!
+                (dc_4link.getCurrentPosition()< fourLinkUpper && dc_4link.getCurrentPosition()>fourLinkLower)){ //
             dc_4link.setPower(gamepad2.right_stick_y);
 
         }
+        dc_sweeper.setPower(gamepad2.left_stick_y);
         leftWheel.setPower(gamepad1.left_stick_y);
         rightWheel.setPower(gamepad1.right_stick_y);
     }
